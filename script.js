01375807c40ca4eddf10ab3d04037276f89e12e7
jason <jasonsampypvt@gmail.com>
2024-07-26 22:57:26 +0530

    usage example update

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import EventHandler from './modules/EventHandler.js'
-import Renderer from './modules/Renderer.js'
 import { Shrike,ShrikeHitbox,ShrikeGate, ShrikeRenderObject,ShrikeLayer,ShrikeTransform } from './modules/Shrike.js';
 
 const canvas = document.getElementById('canvas1')
@@ -10,9 +8,10 @@ const CANVAS_HEIGHT = canvas.height = document.body.clientHeight;
 
 const GameEngine = new Shrike(canvas,1,CANVAS_WIDTH,CANVAS_HEIGHT);
 
-const GameObject = new ShrikeRenderObject('rectangle',{width:101,height:10,color:'#ff4013'})
+const GameObject = new ShrikeRenderObject('rectangle',{width:100,height:100,color:'#ff4013'})
 const Hitbox = new ShrikeHitbox('rectangle',{width:101,height:10,color:'#ff4013'})
 const transformation  = new ShrikeTransform()
+
 GameObject.bindTransformation(transformation)
 Hitbox.bindTransformation(transformation)
 
@@ -21,21 +20,11 @@ const behaviorLayer = new ShrikeLayer('collision')
 const geometryLayer = new ShrikeLayer('render')
 
 geometryLayer.push(GameObject)
-behaviorLayer.push(Hitbox)
+//behaviorLayer.push(Hitbox) // behavior system to be removed unified function for per frame updates and fixed updates to be implemented
+
+transformation.matrix = [1,-1,0,-1,1,0,0,0,1]
 
 const baseLayer  = new ShrikeGate(geometryLayer,behaviorLayer)
-
-Hitbox.onFrame = function(e){
-        transformation.matrix = [1,0,0,0,1,0,e.mouse_x,e.mouse_y,1] 
-    if(e.keydown['a']){
-        transformation.matrix = [1,0,0,0,1,0,0,0,1] 
-    }
-}
-
-
-
-
-
 
 
 GameEngine.activeLayer = baseLayer
