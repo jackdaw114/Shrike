36698f298e6b9222cbb084a6ffc771a3d8d4d2fc
Nigel Colaco <nigel.colaco12@gmail.com>
2025-01-28 18:20:46 +0530

    SGui File Panel joined the dark side.

--- tests.js
+++ tests.js
@@ -7,7 +7,7 @@
 // import DebugSystem from "./src/gpu/debug-helper";
 // import PickingSystem from "./src/gpu/picker";
 // import {MouseEvent} from "./src/event-handler/event-handler";
-import SGui, { SGuiSlider } from "./lib/shrike-gui/sgui";
+import SGui, { SGuiFilePanel, SGuiSlider } from "./lib/shrike-gui/sgui";
 // import PickingSystem from './src/graphics/picker';
 // import { PickingSystem } from './src/gpu/picker/picker';
 // import { DebugSystem } from './src/gpu/debug-graphics/debug-helper';
@@ -22,24 +22,16 @@ const sguiInstance = new SGui();
 
 
 
-let panelHandler = sguiInstance.createWindow("panel1", true)
-let panelHandler2 = sguiInstance.createWindow("panel2", true)
+let panelWindow = sguiInstance.createWindow("panel1", true)
+let panelWindow2 = sguiInstance.createWindow("panel2", true)
 let sliderWindow = sguiInstance.createWindow("slider", true)
 let slider = new SGuiSlider(sliderWindow);
-
-let filePanelController = {
-    files: [],
-    currentContext: null,
-}
-
-let filePanelController2 = {
-    files: [],
-    currentContext: null,
-}
+let filePanel = new SGuiFilePanel(panelWindow);
+let filePanel2 = new SGuiFilePanel(panelWindow2);
 
 
-panelHandler.appendFilePanel(filePanelController);
-panelHandler2.appendFilePanel(filePanelController2);
+panelWindow.appendChild(filePanel);
+panelWindow2.appendFilePanel(filePanel2);
 sliderWindow.appendChild(slider)
 
 
