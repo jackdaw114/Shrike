0a39f7e7b816d8b6d0c70a15716a7e97d9cf5523
Nigel Colaco <nigel.colaco12@gmail.com>
2025-03-24 17:54:43 +0530

    Property window init prototype

--- src/editor/widgets/properties-window.js
+++ src/editor/widgets/properties-window.js
@@ -1,17 +1,26 @@
 import SGuiDropDown from "../../../lib/shrike-gui/child-elements/drop-down";
+import SGuiFilePanel from "../../../lib/shrike-gui/child-elements/file-panel";
 
-export const createPropertiesWindow = (sgui,object) => {
+export const createPropertiesWindow = (sgui, object) => {
     const propertiesWindow = {};
 
-    propertiesWindow.mainWindow = sgui.createWindow("properties",true)
-    propertiesWindow.object = object 
-    propertiesWindow.geometryDropDown = new SGuiDropDown({heading:"geometry"})
-    propertiesWindow.scriptDropDown = new SGuiDropDown({heading:"script"})
+    propertiesWindow.mainWindow = sgui.createWindow("properties", true)
+    propertiesWindow.object = object
+    propertiesWindow.components = [];
+    document.addEventListener("set-active-object", (e) => {
+        propertiesWindow.components = e.detail.entity.components;
+        console.log("propertiesWindow.components  ",propertiesWindow.components)
+        for (const key of Object.keys(propertiesWindow.components)) {
+            console.log("key: ", key)
+            if (key === "Transformation") continue;
+            if (["mainWindow","object"].includes(key)) continue;
+            propertiesWindow.mainWindow.appendChild(propertiesWindow[key])
+            propertiesWindow[key].contentDiv.appendChild(new SGuiFilePanel(propertiesWindow[key]));
+        }
+    })
+    propertiesWindow.Geometry = new SGuiDropDown({heading:"Geometry"})
+    propertiesWindow.Script = new SGuiDropDown({heading:"Script"})
 
-    for (const key of Object.keys(propertiesWindow)) {
-        if (["mainWindow","object"].includes(key)) continue;
-        propertiesWindow.mainWindow.appendChild(propertiesWindow[key]);
-    }
     return propertiesWindow;
 }
 
